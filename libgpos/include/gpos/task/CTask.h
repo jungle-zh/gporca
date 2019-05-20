e5df51a8ed1a7ba2465a6e6488db20871c3743e3
jungle.zhou <jungle.zhou@ucloud.cn>
2019-05-20 14:47:59 +0800

    dbg

--- libgpos/include/gpos/task/CTask.h
+++ libgpos/include/gpos/task/CTask.h
@@ -202,7 +202,9 @@ namespace gpos
 				ULONG trace
 				)
 			{
-				return this->m_task_ctxt->IsTraceSet(trace);
+				//return this->m_task_ctxt->IsTraceSet(trace);
+
+				return  trace > -1;
 			}
 
 			
